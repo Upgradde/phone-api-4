7f6183c73a588fb060dc3655e90677748f94840d
Winfield Little <win@winfieldlittle.com>
2015-04-09 00:17:45 -0500

    Edit monkey file

--- hello-monkey.php
+++ hello-monkey.php
@@ -3,6 +3,6 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>Hello You Monkey You</Say>
+    <Say>Hello Joe Joe the monkey man.  You walk, you talk, you crawl on your belly like ann alligator</Say>
 </Response>
 
